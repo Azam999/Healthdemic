56fe3dd0819248b3ab1ad491f534a6a18f3009ed
Azam Ahmed <aziskool20@gmail.com>
2022-03-18 18:50:34 -0400

    switch to yarn

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,8 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI);
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
