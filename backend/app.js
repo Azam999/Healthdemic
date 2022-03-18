8f44f083e94a61ee45acc9e6a035beae91b18cce
Azam Ahmed <aziskool20@gmail.com>
2022-03-18 18:31:05 -0400

    Set up basic app.js

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const router = express.Router();
+
+// Initialize dotenv
+dotenv.config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+router.use('/', indexRouter);
+router.use('/users', usersRouter);
+app.use('/api', router);
+app.use('*', (req, res) => {
+  res.status(404).send({
+    message: 'Not Found',
+  });
+});
+
+module.exports = app;
