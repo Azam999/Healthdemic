aaafc0dcb833e03d6343247caa473b384b5c6033
Azam Ahmed <aziskool20@gmail.com>
2022-03-19 16:57:27 -0400

    Fix formatting

--- backend/routes/features.js
+++ backend/routes/features.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { spawn } = require('child_process');
-const {PythonShell} = require('python-shell');
+const { PythonShell } = require('python-shell');
 
 router.get('/diet-generator', function (req, res) {
     let { weight, height, age, gender, phys_act } = req.query;
@@ -11,12 +11,12 @@ router.get('/diet-generator', function (req, res) {
         pythonPath: 'python3',
         pythonOptions: ['-u'], // get print results in real-time
         scriptPath: '../python',
-        args: [weight, height, age , gender, phys_act]
+        args: [weight, height, age, gender, phys_act],
     };
 
     PythonShell.run('diet_generator.py', options, function (err, results) {
         if (err) throw err;
-        
+
         res.send(results[0]);
     });
 });
