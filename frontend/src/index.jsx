36901ee92b53738e8f995997f8b2fb1e5ead4e04
bvmble <alexluo92823@gmail.com>
2022-03-18 20:30:22 -0400

    add login and register pages

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import Error404Page from "./pages/Error404Page";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/register" element={<RegisterPage />} />
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route path="*" element={<Error404Page />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
