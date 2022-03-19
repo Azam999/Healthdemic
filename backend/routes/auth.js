3dba1c6190bc778e6f0f63c9f30c2898ffa6c837
Azam Ahmed <aziskool20@gmail.com>
2022-03-18 21:17:02 -0400

    Add login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,8 +9,6 @@ router.post('/register', async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  console.log(user);
-
   if (user) {
     return res.status(409).json({ message: 'User already exists' });
   }
@@ -43,6 +41,66 @@ router.post('/register', async (req, res) => {
       auth: true,
     });
   });
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    if (!email) {
+      return res.status(401).json({
+        message: 'Please enter an email',
+        code: res.statusCode,
+      });
+    } else if (!password) {
+      return res.status(401).json({
+        message: 'Please enter a password',
+        code: res.statusCode,
+      });
+    } else {
+      return res.status(401).json({
+        message: 'Please enter both the email and password',
+        code: res.statusCode,
+      });
+    }
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      message: 'An account with that email does not exist',
+      code: res.statusCode,
+    });
+  }
+
+  const passwordVerified = await bcrypt.compare(password, user.password);
+
+  const jwtPayload = {
+    _id: user._id,
+  };
+
+  if (passwordVerified) {
+    const accessToken = jwt.sign(jwtPayload, process.env.JWT_KEY, {
+      expiresIn: '30d',
+    });
+
+    res.status(201).send({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      accessToken,
+      message: 'Authenticated',
+      auth: true,
+    });
+  } else {
+    res.status(401).json({
+      message: 'Not Authenticated',
+      auth: false,
+    });
+  }
 });
 
 module.exports = router;
